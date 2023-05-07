0efcf2e04024103ac0985ffab9402bf6ab73dfdb
EdivaldoCostaLimaJr <edivaldocostadv@gmail.com>
2023-05-07 00:06:11 -0300

    first commit

---
+++ src/main/java/com/example/Projeto_Dev_Mobile/ProjetoDevMobileApplication.java
@@ -0,0 +1,13 @@
+package com.example.Projeto_Dev_Mobile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoDevMobileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoDevMobileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Projeto_Dev_Mobile/ProjetoDevMobileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Projeto_Dev_Mobile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoDevMobileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
